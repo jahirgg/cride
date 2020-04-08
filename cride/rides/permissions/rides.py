069093de2b3f4154b97c731f68fa4ddf4d11a695
Jahir Gonzalez <jahir.gonzalez@mibus.com.pa>
2020-04-08 18:38:46 -0500

    Avances hasta el video 47

---
+++ cride/rides/permissions/rides.py
@@ -0,0 +1,12 @@
+"""Ride Permissions."""
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+
+class IsRideOwner(BasePermission):
+    """Verify requesting user is the ride creator."""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify that the requesting user is the ride creator."""
+        return request.user == obj.offered_by
--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -7,6 +7,7 @@ from rest_framework.generics import get_object_or_404
 # Permissions
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.rides.permissions.rides import IsRideOwner
 
 # Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -26,10 +27,9 @@ from django.utils import timezone
 
 class RideViewSet(mixins.CreateModelMixin,
                   mixins.ListModelMixin,
+                  mixins.UpdateModelMixin,
                   viewsets.GenericViewSet):
     """Ride view set."""
-
-    permission_classes = [IsAuthenticated, IsActiveCircleMember]
 
     filter_backends = (SearchFilter, OrderingFilter)
     ordering = ('departure_date', 'arrival_date', 'available_seats')
@@ -44,6 +44,13 @@ class RideViewSet(mixins.CreateModelMixin,
             slug_name=slug_name
         )
         return super(RideViewSet, self).dispatch(request, *args, **kwargs)
+
+    def get_permissions(self):
+        """Assign permission based on actions."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsRideOwner)
+        return [permission() for permission in permissions]
 
     def get_serializer_context(self):
         """Add circle to serializer context."""
@@ -60,6 +67,9 @@ class RideViewSet(mixins.CreateModelMixin,
     def get_queryset(self):
         """Return active circle's rides."""
         offset = timezone.now() + timedelta(minutes=10)
+        ##import pdb; pdb.set_trace()
+        if self.action == 'update':
+            return self.circle.ride_set.all()
         return self.circle.ride_set.filter(
             departure_date__gte=offset,
             is_active=True,
